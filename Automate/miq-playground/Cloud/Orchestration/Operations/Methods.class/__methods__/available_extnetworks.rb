d47314ca3f0ce381fd22940aedfc78142f9fe910
Christian Jung <chris@jung-christian.de>
2017-10-04 23:03:38 +0200

    find external networks

--- Automate/miq-playground/Cloud/Orchestration/Operations/Methods.class/__methods__/available_extnetworks.rb
+++ Automate/miq-playground/Cloud/Orchestration/Operations/Methods.class/__methods__/available_extnetworks.rb
@@ -1,5 +1,5 @@
 #
-# Description: provide the dynamic list content from available external networks
+# Description: provide the dynamic list content from external networks
 #
 
 #$evm.instantiate('/Discovery/ObjectWalker/object_walker')
@@ -12,6 +12,24 @@ list = {}
 tenant_name = $evm.root['dialog_tenant_name']
 if tenant_name.blank?
   list['unspecified']="select tenant first"
+else
+  providers = $evm.vmdb("ext_management_system").all
+  providers.each { |provider| 
+    $evm.log("info", "current provider: #{provider.inspect}")
+    if provider.type == "ManageIQ::Providers::Openstack::NetworkManager"
+      $evm.log("info", "Provider #{provider.name} seems to be an OpenStack Network Provider, getting list of private networks aka external_networks...")
+      $evm.log("info", "Hosts: #{provider.hosts}")
+      if provider.hosts.length > 0 
+        $evm.log("info", "Providers returns more than one host, this must be an UnderCloud, skipping")
+        next
+      end 
+      external_networks = provider.public_networks
+      external_networks.each { |external_network|
+        $evm.log("info", "Found external_network: #{external_network.name} with ID #{external_network.ems_ref}")
+        list[external_network.ems_ref]="#{external_network.name} on #{provider.name}"
+      }
+    end
+  }
 end
 
 dialog_field = $evm.object 
