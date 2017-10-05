739b8d6225a198b628a2e588118a4bc5108b1a8f
Christian Jung <chris@bolzchristian.de>
2017-10-05 17:15:33 +0200

    new approach

--- Automate/miq-playground/Cloud/Orchestration/Operations/Methods.class/__methods__/available_subnets.rb
+++ Automate/miq-playground/Cloud/Orchestration/Operations/Methods.class/__methods__/available_subnets.rb
@@ -9,28 +9,40 @@ $evm.root.attributes.sort.each { |k, v| $evm.log("info", "Root:<$evm.root> Attri
 
 list = {}
 
-tenant_id = $evm.root['dialog_tenant_id']
-if tenant_id.blank?
-  list['unspecified']="select tenant first"
-else
-  tenant = $evm.vmdb("cloud_tenant").find_by_id(tenant_id)
-  $evm.log("info", "Found tenant #{tenant.name} by ID #{tenant_id}")
+external_network_id = $evm.root['dialog_param_external_network_id:']
 
-  provider = tenant.ext_management_system
-  $evm.log("info", "Found provider #{provider.name} from tenant relationship")
+external_network = $evm.vmdb("cloud_networks").where(:ems_ref => external_network_id)
 
-  $evm.log("info", "current provider: #{provider.inspect}")
-  if provider.type == "ManageIQ::Providers::Openstack::CloudManager"
-    $evm.log("info", "Provider #{provider.name} seems to be an OpenStack Provider")
+$evm.log("info", "Found external_network #{external_network.inspect} by ems_ref #{external_network_id}")
 
-    $evm.log("info", "getting list of private networks aka subnets...")
-    subnets = provider.cloud_subnets
-    subnets.each { |subnet|
-      $evm.log("info", "Found Subnet: #{subnet.name} with ID #{subnet.ems_ref} and details: #{subnet.inspect}")
-      list[subnet.ems_ref]="#{subnet.name} on #{provider.name}"
-    }
-  end
-end 
+#
+#
+#
+#
+
+# tenant_id = $evm.root['dialog_tenant_id']
+# if tenant_id.blank?
+#   list['unspecified']="select tenant first"
+# else
+#   tenant = $evm.vmdb("cloud_tenant").find_by_id(tenant_id)
+#   $evm.log("info", "Found tenant #{tenant.name} by ID #{tenant_id}")
+
+#   provider = tenant.ext_management_system
+#   $evm.log("info", "Found provider #{provider.name} from tenant relationship")
+
+#   $evm.log("info", "current provider: #{provider.inspect}")
+#   if provider.type == "ManageIQ::Providers::Openstack::CloudManager"
+#     $evm.log("info", "Provider #{provider.name} seems to be an OpenStack Provider")
+
+#     $evm.log("info", "getting list of private networks aka subnets...")
+#     $evm.log("info", "Output of tenant: #{tenant.inspect}")
+#     subnets = provider.cloud_subnets
+#     subnets.each { |subnet|
+#       $evm.log("info", "Found Subnet: #{subnet.name} with ID #{subnet.ems_ref} and details: #{subnet.inspect}")
+#       list[subnet.ems_ref]="#{subnet.name} on #{provider.name}"
+#     }
+#   end
+# end 
 
 dialog_field = $evm.object 
 
