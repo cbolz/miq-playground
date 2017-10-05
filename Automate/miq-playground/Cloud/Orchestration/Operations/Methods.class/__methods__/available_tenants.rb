1898d15fc12b344881f25c39e97b3cb83e020efa
Christian Jung <chris@jung-christian.de>
2017-10-05 13:11:29 +0200

    getting closer

--- Automate/miq-playground/Cloud/Orchestration/Operations/Methods.class/__methods__/available_tenants.rb
+++ Automate/miq-playground/Cloud/Orchestration/Operations/Methods.class/__methods__/available_tenants.rb
@@ -9,30 +9,33 @@ $evm.root.attributes.sort.each { |k, v| $evm.log("info", "Root:<$evm.root> Attri
 
 list = {}
 
-providers = $evm.vmdb("ext_management_system").all
-providers.each { |provider| 
-  if provider.type == "ManageIQ::Providers::Openstack::NetworkManager"
-    $evm.log("info", "Provider #{provider.name} seems to be an OpenStack Network Provider, getting list tenants...")
-    $evm.log("info", "Hosts: #{provider.hosts}")
-    if provider.hosts.length > 0 
-      $evm.log("info", "Providers returns more than one host, this must be an UnderCloud, skipping")
-      next
-    end 
+# providers = $evm.vmdb("ext_management_system").all
+# providers.each { |provider| 
+#   if provider.type == "ManageIQ::Providers::Openstack::NetworkManager"
+#     $evm.log("info", "Provider #{provider.name} seems to be an OpenStack Network Provider, getting list tenants...")
+#     $evm.log("info", "Hosts: #{provider.hosts}")
+#     if provider.hosts.length > 0 
+#       $evm.log("info", "Providers returns more than one host, this must be an UnderCloud, skipping")
+#       next
+#     end 
 
-    $evm.log("info", "current provider: #{provider.inspect}")
-    # tenants = provider.tenant
-    # tenants.each { |tenant|
-    #   $evm.log("info", "External Network: #{tenant.inspect}")
-    #   $evm.log("info", "Found external_network: #{tenant.name}")
-    #   list[tenant.ems_ref]="#{tenant.name} on #{provider.name}"
-    # }
-  end
-}
+#     $evm.log("info", "current provider: #{provider.inspect}")
+#     # tenants = provider.tenant
+#     # tenants.each { |tenant|
+#     #   $evm.log("info", "External Network: #{tenant.inspect}")
+#     #   $evm.log("info", "Found external_network: #{tenant.name}")
+#     #   list[tenant.ems_ref]="#{tenant.name} on #{provider.name}"
+#     # }
+#   end
+# }
 
 $evm.log("info", "Retrieving list of cloud tenants...")
 tenants = $evm.vmdb("cloud_tenant").all
 tenants.each { |tenant|
   $evm.log("info", "Current tenant: #{tenant.inspect}")
+  provider = tenant.ext_management_system
+  $evm.log("info", "Tenant on provider #{provider.name}")
+  list[tenant.id]="#{tenant.name} on #{provider.name}"
 }
 
 # external_networks = $evm.vmdb("cloud_networks").all
