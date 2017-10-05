83c48dba33c95dc9f2778074731a4be50175dc28
Christian Jung <chris@bolzchristian.de>
2017-10-05 21:13:46 +0200

    using FOG now

--- Automate/miq-playground/Cloud/Orchestration/Operations/Methods.class/__methods__/available_subnets.rb
+++ Automate/miq-playground/Cloud/Orchestration/Operations/Methods.class/__methods__/available_subnets.rb
@@ -9,44 +9,83 @@ $evm.root.attributes.sort.each { |k, v| $evm.log("info", "Root:<$evm.root> Attri
 
 list = {}
 
-external_networks = $evm.vmdb("cloud_network").all
-
 tenant_id = $evm.root['dialog_tenant_id']
 if tenant_id.blank?
   list['unspecified']="select tenant first"
 else
-  subnets = $evm.vmdb("cloud_subnet").all 
-  subnets.each { |subnet|
-    $evm.log("info", "Subnet: #{subnet.inspect}")
+  tenant = $evm.vmdb("cloud_tenant").find_by_id(tenant_id)
+  $evm.log("info", "Found tenant #{tenant.name} with ems_ref #{tenant.ems_ref} by ID #{tenant_id}")
+
+  provider = tenant.ext_management_system
+  $evm.log("info", "Found provider #{provider.name} from tenant relationship")
+
+  external_networks = provider.cloud_networks
+
+  require 'rest-client'
+  require 'json'
+  require 'fog/openstack'
+
+  credentials={
+    :provider => "OpenStack",
+    :openstack_api_key => provider.authentication_password,
+    :openstack_username => provider.authentication_userid,
+    :openstack_auth_url => "http://#{provider.hostname}:#{provider.port}/v3/auth/tokens",
+    :openstack_project_name => tenant.name,
+    :openstack_domain_name => provider.name
   }
 
-  tenants = $evm.vmdb("cloud_tenant").all 
-  tenants.each { |tenant|
-    $evm.log("info", "Tenant: #{tenant.inspect}")
+  $evm.log("info", "Credentials: #{credentials.inspect}")
+
+  network = Fog::Network.new(credentials)
+  subnets = network.list_subnets
+  $evm.log("info", "External Networks from FOG: #{subnets.body}")
+
+  networks = subnets.body["networks"]
+  networks.each { |network|
+    $evm.log("info", "Current network: #{network.inspect}")
+    $evm.log("info", "Network is an external network, adding it to the list")
+    networkname = network["name"]
+    list[network["id"]]="#{networkname} on Provider #{provider.name}"
   }
 
-  cloud_networks = $evm.vmdb("cloud_network").all 
-  cloud_networks.each { |cloud_network|
-    $evm.log("info", "Cloud network: #{cloud_network.inspect}")
-    $evm.log("info", "CLoud network associated tenant: #{cloud_network.cloud_tenant.inspect}")
-    $evm.log("info", "CLoud network associated subnets: #{cloud_network.cloud_subnets.inspect}")
-  }
+# external_networks = $evm.vmdb("cloud_network").all
 
-end 
+# tenant_id = $evm.root['dialog_tenant_id']
+# if tenant_id.blank?
+#   list['unspecified']="select tenant first"
+# else
+#   subnets = $evm.vmdb("cloud_subnet").all 
+#   subnets.each { |subnet|
+#     $evm.log("info", "Subnet: #{subnet.inspect}")
+#   }
 
-cloud_network = nil
+#   tenants = $evm.vmdb("cloud_tenant").all 
+#   tenants.each { |tenant|
+#     $evm.log("info", "Tenant: #{tenant.inspect}")
+#   }
 
-if cloud_network.nil?
-  $evm.log("info", "Failed to find selected network")
-  list['unspecified']="select external network first"
-else 
-  $evm.log("info", "Found external_network #{cloud_network.inspect} by ems_ref #{external_network_id}")
+#   cloud_networks = $evm.vmdb("cloud_network").all 
+#   cloud_networks.each { |cloud_network|
+#     $evm.log("info", "Cloud network: #{cloud_network.inspect}")
+#     $evm.log("info", "CLoud network associated tenant: #{cloud_network.cloud_tenant.inspect}")
+#     $evm.log("info", "CLoud network associated subnets: #{cloud_network.cloud_subnets.inspect}")
+#   }
 
-  cloud_network.cloud_tenants.each { |subnet|
-    $evm.log("info", "Adding subnet: #{subnet.name} with ems_ref #{subnet.ems_ref}")
-    list[subnet.ems_ref]="#{subnet.name}"
-  }
-end 
+# end 
+
+# cloud_network = nil
+
+# if cloud_network.nil?
+#   $evm.log("info", "Failed to find selected network")
+#   list['unspecified']="select external network first"
+# else 
+#   $evm.log("info", "Found external_network #{cloud_network.inspect} by ems_ref #{external_network_id}")
+
+#   cloud_network.cloud_tenants.each { |subnet|
+#     $evm.log("info", "Adding subnet: #{subnet.name} with ems_ref #{subnet.ems_ref}")
+#     list[subnet.ems_ref]="#{subnet.name}"
+#   }
+# end 
 
 dialog_field = $evm.object 
 
