cb93e77494cc585c702f16a0315171dc581f5d06
Christian Jung <chris@jung-christian.de>
2017-10-05 11:37:45 +0200

    adding my override for tenant list

---
+++ Automate/miq-playground/Cloud/Orchestration/Operations/Methods.class/__methods__/available_tenants.rb
@@ -0,0 +1,52 @@
+#
+# Description: provide the dynamic list content from available tenants
+#
+
+$evm.instantiate('/Discovery/ObjectWalker/object_walker')
+
+# Dump all of root's attributes to the log
+$evm.root.attributes.sort.each { |k, v| $evm.log("info", "Root:<$evm.root> Attribute - #{k}: #{v}")}
+
+list = {}
+
+tenatns = $evm.vmdb("ext_management_system").all
+providers.each { |provider| 
+  #$evm.log("info", "current provider: #{provider.inspect}")
+  if provider.type == "ManageIQ::Providers::Openstack::NetworkManager"
+    $evm.log("info", "Provider #{provider.name} seems to be an OpenStack Network Provider, getting list of private networks aka external_networks...")
+    $evm.log("info", "Hosts: #{provider.hosts}")
+    if provider.hosts.length > 0 
+      $evm.log("info", "Providers returns more than one host, this must be an UnderCloud, skipping")
+      next
+    end 
+    tenants = provider.cloud_tenants
+    tenants.each { |tenant|
+      $evm.log("info", "External Network: #{tenant.inspect}")
+      $evm.log("info", "Found external_network: #{tenant.name}")
+      list[tenant.ems_ref]="#{tenant.name} on #{provider.name}"
+    }
+  end
+}
+
+# external_networks = $evm.vmdb("cloud_networks").all
+# external_networks.each { |external_network|
+#   $evm.log("info", "Finding cloud networks: #{external_network.inspect}")
+# }
+
+
+dialog_field = $evm.object 
+
+# sort_by: value / description / none
+dialog_field["sort_by"] = "description"
+
+# sort_order: ascending / descending
+dialog_field["sort_order"] = "ascending"
+
+# data_type: string / integer
+dialog_field["data_type"] = "string"
+
+# required: true / false
+dialog_field["required"] = "false"
+
+dialog_field["values"] = list
+dialog_field["default_value"] = nil
