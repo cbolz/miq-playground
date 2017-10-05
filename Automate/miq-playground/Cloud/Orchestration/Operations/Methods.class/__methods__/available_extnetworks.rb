3a9a56b59f4eb61a590bb9e02847105b7555a598
Christian Jung <chris@bolzchristian.de>
2017-10-05 16:23:02 +0200

    more debug code

--- Automate/miq-playground/Cloud/Orchestration/Operations/Methods.class/__methods__/available_extnetworks.rb
+++ Automate/miq-playground/Cloud/Orchestration/Operations/Methods.class/__methods__/available_extnetworks.rb
@@ -23,7 +23,8 @@ else
   external_networks.each { |external_network|
     $evm.log("info", "External Network: #{external_network.inspect}")
     $evm.log("info", "Found external_network: #{external_network.name} with ID #{external_network.ems_ref} and cloud_tenant #{external_network.cloud_tenant.inspect}")
-    if external_network.cloud_tenant_id == tenant_id
+    $evm.log("info", "Checking cloud_tenant from external_network: #{external_network.cloud_tenant.inspect}")
+    if external_network.cloud_tenant == tenant_id
       $evm.log("info", "Adding network to dialog, tenant ID does match")
       list[external_network.ems_ref]="#{external_network.name} on #{provider.name}"
     else 
