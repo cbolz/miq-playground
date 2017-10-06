56d9a133ee73f687410779c5db227aca39b701f2
Christian Jung <chris@jung-christian.de>
2017-10-06 13:57:02 +0200

    first draft to populate tenant name

--- Automate/miq-playground/Cloud/Orchestration/Provisioning/StateMachines/Methods.class/__methods__/PopulateTenantName.rb
+++ Automate/miq-playground/Cloud/Orchestration/Provisioning/StateMachines/Methods.class/__methods__/PopulateTenantName.rb
@@ -6,3 +6,20 @@ $evm.instantiate('/Discovery/ObjectWalker/object_walker')
 
 # Dump all of root's attributes to the log
 $evm.root.attributes.sort.each { |k, v| $evm.log("info", "Root:<$evm.root> Attribute - #{k}: #{v}")}
+
+prov = @handle.root['service_template_provision_task']
+
+$evm.log("info", "Provisioning object: #{prov.inspect}")
+
+tenant_id = prov.get_option("dialog_tenant_id")
+
+# find tenant by ID
+if tenant_id.blank?
+    list['unspecified']="select tenant first"
+else
+    tenant = $evm.vmdb("cloud_tenant").find_by_id(tenant_id)
+    $evm.log("info", "Found tenant by ID #{tenant_id}: #{tenant.inspect}")
+
+    prov.set_option("dialog_tenant_name", "#{tenant.name}")
+    prov.set_option("tenant_name", "#{tenant.name}")
+end
