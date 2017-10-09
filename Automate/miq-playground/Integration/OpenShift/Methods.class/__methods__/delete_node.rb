f87faf3504b719fb481733c1fc0d301f1ba30a0a
Christian Jung <chris@bolzchristian.de>
2017-10-09 13:39:03 +0200

    initial import

---
+++ Automate/miq-playground/Integration/OpenShift/Methods.class/__methods__/delete_node.rb
@@ -0,0 +1,40 @@
+#
+#            Automate Method
+#
+# Copyright (C) 2016, Christian Jung
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+begin
+    $evm.log("info", "EVM Automate Method Started")
+  
+    # Dump all of root's attributes to the log
+    $evm.root.attributes.sort.each { |k, v| $evm.log("info", "Root:<$evm.root> Attribute - #{k}: #{v}")}
+  
+    rc=system("/root/add_host_v2.sh")
+    $evm.log("info", "Return Code: #{rc}")
+
+    #
+    # Exit method
+    #
+    $evm.log("info", "EVM Automate Method Ended")
+    exit MIQ_OK
+  
+    #
+    # Set Ruby rescue behavior
+    #
+rescue => err
+    $evm.log("error", "[#{err}]\n#{err.backtrace.join("\n")}")
+    exit MIQ_ABORT
+end
+  
