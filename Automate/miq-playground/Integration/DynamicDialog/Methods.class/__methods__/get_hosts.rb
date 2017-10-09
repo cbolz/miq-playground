9d3e68cea2ec8714203e4844f1a142e70b98c23f
Christian Jung <chris@bolzchristian.de>
2017-10-09 17:14:19 +0200

    filter only nodes

--- Automate/miq-playground/Integration/DynamicDialog/Methods.class/__methods__/get_hosts.rb
+++ Automate/miq-playground/Integration/DynamicDialog/Methods.class/__methods__/get_hosts.rb
@@ -32,7 +32,7 @@ begin
         $evm.log("info", "User selected provider #{ems.name}: #{ems.inspect} with hosts: #{ems.vms.inspect}")
         ems.vms.each { |vm|
             $evm.log("info", "Adding Host with ID #{vm.id} and name #{vm.name}")
-            list[vm.id]=vm.name
+            list[vm.id]=vm.name if vm.name =~ /^node/
         }
     end 
 
