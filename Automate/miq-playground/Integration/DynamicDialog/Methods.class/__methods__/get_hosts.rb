7b7d5ad3316d7a3b57226c9c1148d03f54f838da
Christian Jung <chris@bolzchristian.de>
2017-10-09 16:51:46 +0200

    getting list of hosts from selected provider

--- Automate/miq-playground/Integration/DynamicDialog/Methods.class/__methods__/get_hosts.rb
+++ Automate/miq-playground/Integration/DynamicDialog/Methods.class/__methods__/get_hosts.rb
@@ -21,6 +21,33 @@ begin
     # Dump all of root's attributes to the log
     $evm.root.attributes.sort.each { |k, v| $evm.log("info", "Root:<$evm.root> Attribute - #{k}: #{v}")}
   
+    provider_id = $evm.root["dialog_provider_id"]
+
+    list = {}
+
+    ems = $evm.vmdb("ext_management_system").find_by_id(provider_id)
+    ems.hosts.all.each { |host|
+        $evm.log("info", "Adding Host with ID #{host.id} and name #{host.name}")
+        list[host.id]=host.name
+    }
+
+    dialog_field = $evm.object 
+    
+    # sort_by: value / description / none
+    dialog_field["sort_by"] = "description"
+    
+    # sort_order: ascending / descending
+    dialog_field["sort_order"] = "ascending"
+    
+    # data_type: string / integer
+    dialog_field["data_type"] = "string"
+    
+    # required: true / false
+    dialog_field["required"] = "false"
+    
+    dialog_field["values"] = list
+    dialog_field["default_value"] = nil
+
     #
     # Exit method
     #
