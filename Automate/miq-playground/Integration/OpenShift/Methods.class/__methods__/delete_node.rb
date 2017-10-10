828754afb47db81a1b1e0a9aa70674128c5ee2f2
Christian Jung <chris@bolzchristian.de>
2017-10-10 11:05:18 +0200

    fix error handling and debugging

--- Automate/miq-playground/Integration/OpenShift/Methods.class/__methods__/delete_node.rb
+++ Automate/miq-playground/Integration/OpenShift/Methods.class/__methods__/delete_node.rb
@@ -25,14 +25,13 @@ begin
     
     nodename.each { |node|
         random = (0...8).map { (65 + rand(26)).chr }.join    
-        $evm.log("info", "su - clouduser -c '/home/clouduser/delete_host.sh #{nodename}' &> /tmp/delete-#{random}.log")
+        $evm.log("info", "su - clouduser -c '/home/clouduser/delete_host.sh #{node}' &> /tmp/delete-#{random}.log")
         rc=system("su - clouduser -c '/home/clouduser/delete_host.sh #{node}' &> /tmp/delete-#{random}.log")
         $evm.log("info", "Return Code: #{rc.inspect}")    
+        if rc != true
+            exit MIQ_ABORT
+        end 
     }
-    
-    if rc != true
-        exit MIQ_ABORT
-    end 
 
     #
     # Exit method
