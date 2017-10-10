3b231c2565d55b5a884ab371e4ab346d4303e0fe
Christian Jung <chris@bolzchristian.de>
2017-10-10 10:08:51 +0200

    call the script for each node

--- Automate/miq-playground/Integration/OpenShift/Methods.class/__methods__/delete_node.rb
+++ Automate/miq-playground/Integration/OpenShift/Methods.class/__methods__/delete_node.rb
@@ -26,9 +26,12 @@ begin
     random = (0...8).map { (65 + rand(26)).chr }.join
 
     $evm.log("info", "su - clouduser -c '/home/clouduser/delete_host.sh #{nodename}' &> /tmp/delete-#{random}.log")
-    #rc=system("su - clouduser -c '/home/clouduser/delete_host.sh #{nodename}' &> /tmp/delete-#{random}.log")
-    $evm.log("info", "Return Code: #{rc.inspect}")
-
+    
+    nodename.each { |node|
+        rc=system("su - clouduser -c '/home/clouduser/delete_host.sh #{node}' &> /tmp/delete-#{random}.log")
+        $evm.log("info", "Return Code: #{rc.inspect}")
+    }
+    
     if rc != true
         exit MIQ_ABORT
     end 
